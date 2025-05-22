c17e4577c01ee53a0dde6bca279182a873e9a2e0
Ryan Giles <rgiles0820@gmail.com>
2025-05-22 12:20:39 -0400

    adds about

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,7 +3,22 @@ import "./About.scss";
 function About() {
   return (
     <div className="about">
-      <h1 className="about-title">About Me Page</h1>
+      <div className="about__column">
+        <p className="about__item">About me description</p>
+        <img
+          src="About/about01.jpeg"
+          alt="gaming with spock"
+          className="about__item"
+        />
+      </div>
+      <div className="about__column">
+        <img src="About/about02.jpg" alt="headshot" className="about__item" />
+        <img
+          src="About/about03.jpeg"
+          alt="starwars with dad"
+          className="about__item"
+        />
+      </div>
     </div>
   );
 }
