9edf299b9dd3825edf0c40c2066d55dd06cda6b2
Ryan Giles <rgiles0820@gmail.com>
2025-05-28 16:38:24 -0400

    adds header content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,25 @@
 import "./Footer.scss";
+import instaIcon from "../../Assets/Icons/instagram_icon.png";
+import linkedinIcon from "../../Assets/Icons/linkedin_icon.png";
 
 function Footer() {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <a
+        href="https://www.instagram.com/aurorahull.design/"
+        className="footer__link"
+      >
+        <img src={instaIcon} alt="instagram" className="footer__icon" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/aurora-hull/"
+        className="footer__link"
+      >
+        <img src={linkedinIcon} alt="linkedIn" className="footer__icon" />
+      </a>
+      <p className="footer__text">©Copyright Aurora Hull 2025</p>
+    </footer>
+  );
 }
 
 export default Footer;
